591897db5237941c5b5878b4219fcf20e3e2202d
Roshanvj11 <sroshanvijay@gmail.com>
2024-09-22 19:06:20 +0530

    adding some animation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Button from '@mui/material/Button';
 import "../css/Home.css"
 
 export default function Home() {
@@ -6,24 +7,24 @@ export default function Home() {
     <div className='Home'>
 
       <div className='homeContent'>
-        <div className='headings'>
+        <div className='headings first'>
           <h1 >Hi There,</h1>
         </div>
-        <div className='headings'>
+        <div className='headings second'>
           <h1 >I'm Roshan Vijay</h1>
         </div>
-        <div className='headings'>
+        <div className='headings third'>
           <h1 >A Full web Developer</h1>
         </div>
         <div className='homeButton'>
-          <button>Hire Me</button>
-          <button>Resume</button>
+        <Button className='btn1' variant="contained">Hire Me</Button>
+        <Button className='btn2' variant="contained">Resume</Button>
         </div>
         <div className='homeNavigation'>
-          <p>in</p>
-          <p>git</p>
-          <p>mail</p>
-          <p>fb</p>
+          <p className='para1'>in</p>
+          <p className='para2'>git</p>
+          <p className='para2'>mail</p>
+          <p className='para3'>fb</p>
         </div>
       </div>
 
