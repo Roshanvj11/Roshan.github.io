6329a42f84b63b5b87e6aa770b2b8e4030f55440
Roshanvj11 <sroshanvijay@gmail.com>
2024-09-23 13:49:25 +0530

    new animation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import Button from '@mui/material/Button';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import EmailIcon from '@mui/icons-material/Email';
+import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
 import "../css/Home.css"
 
 export default function Home() {
@@ -17,21 +21,34 @@ export default function Home() {
           <h1 >A Full web Developer</h1>
         </div>
         <div className='homeButton'>
-        <Button className='btn1' variant="contained">Hire Me</Button>
-        <Button className='btn2' variant="contained">Resume</Button>
+          <Button className='btn1' variant="contained">Hire Me</Button>
+          <Button className='btn2' variant="contained">Resume</Button>
         </div>
         <div className='homeNavigation'>
-          <p className='para1'>in</p>
-          <p className='para2'>git</p>
-          <p className='para2'>mail</p>
-          <p className='para3'>fb</p>
+          <a href='https://www.linkedin.com/in/roshan-443070262/' className='para1'>
+            <LinkedInIcon style={{fontSize:'40px' ,color:'#0077B5'}} />
+          </a>
+          <a href='https://github.com/Roshanvj11' className='para2'>
+            <GitHubIcon style={{fontSize:'40px' ,color:'#171515'}} />
+          </a>
+          <a href='https://mail.google.com/mail/?view=cm&to=sroshanvijay@gmail.com&su=Subject&body=BodyText' className='para2'>
+            <EmailIcon style={{fontSize:'40px' ,color:'FCCD2A'}} />
+          </a>
+          <a href='https://m.facebook.com/profile.php?id=100009256512718' className='para3'>
+            <FacebookOutlinedIcon style={{fontSize:'40px' ,color:'#316FF6'}} />
+          </a>
         </div>
       </div>
 
       <div className='homeImage'>
+
         <div className='homeImage2'>
-          <image>image</image>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
         </div>
+
       </div>
 
     </div>
